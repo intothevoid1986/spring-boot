e22208ef3c0ea0557fbe7423f4f0c0a2535b772d
Andrea Colaiuda <andrea.colaiuda@neen.com>
2022-12-01 20:05:16 +0100

    fix(users): sequence and gen value strategy

--- src/main/java/it/irideos/metrics/models/User.java
+++ src/main/java/it/irideos/metrics/models/User.java
@@ -4,13 +4,18 @@ import org.springframework.data.annotation.Id;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.SequenceGenerator;
 import lombok.Data;
 
-@Entity(name = "user")
+@Entity(name = "users")
 @Data
 public class User {
 
 	@Id
+	@SequenceGenerator(name = "users_sequence", sequenceName = "users_sequence", allocationSize = 1)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "users_sequence")
 	private Long id;
 
 	@Column(name = "name")
