37f47dd59fea2d5dd9e2831572dcc67638e183a3
Andrea Colaiuda <andrea.colaiuda@neen.com>
2022-12-02 10:24:50 +0100

    feat(User): generate all args constructor via Lombok

--- src/main/java/it/irideos/metrics/models/User.java
+++ src/main/java/it/irideos/metrics/models/User.java
@@ -6,10 +6,12 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.SequenceGenerator;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Entity(name = "users")
 @Data
+@AllArgsConstructor
 public class User {
 
 	@Id
