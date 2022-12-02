a1315746ec23b8f392066abc292d7cb109738a43
Andrea Colaiuda <andrea.colaiuda@neen.com>
2022-12-02 09:08:23 +0100

    fix(users): changed to jakarta deps

--- src/main/java/it/irideos/metrics/models/User.java
+++ src/main/java/it/irideos/metrics/models/User.java
@@ -1,11 +1,10 @@
 package it.irideos.metrics.models;
 
-import org.springframework.data.annotation.Id;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.SequenceGenerator;
 import lombok.Data;
 
